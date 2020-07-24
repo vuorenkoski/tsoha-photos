66bfeb69267dc141eede75bcdb93b112c2bc5a1e
Lauri Vuorenkoski <lauri.vuorenkoski@helsinki.fi>
2020-07-24 14:25:02 +0300

    Kuvan lataaminen

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, request, session, render_template, redirect
+from flask import Flask, request, session, render_template, redirect, url_for
 from os import getenv
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from PIL import Image
+import os
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["PHOTOFOLDER"] = getenv("PHOTOFOLDER")
+app.config["MAX_CONTENT_PATH"] = 5000000000
 db = SQLAlchemy(app)
-
 
 @app.route("/")
 def index():
@@ -19,22 +22,18 @@ def login():
 
 @app.route("/logindata",methods=["POST"])
 def logindata():
-    if "error" in session:
-        session.pop("error")
     username = request.form["username"]
     password = request.form["password"]
-    sql = "SELECT tunnus, salasana FROM photo_kayttajat WHERE tunnus=:username"
+    sql = "SELECT tunnus, salasana FROM photos_kayttajat WHERE tunnus=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user == None:
-        session["error"]="tunnusta ei ole olemassa"
-        return redirect("/login")
+        return render_template("login.html", error="tunnusta ei ole olemassa")
     if check_password_hash(user[1],password):
         session["username"] = user[0]
         return redirect("/")
 
-    session["error"]="salasana on väärin"
-    return redirect("/login")
+    return render_template("login.html", error="salasana on väärin")
 
 @app.route("/signup")
 def signup():
@@ -42,30 +41,53 @@ def signup():
 
 @app.route("/signupdata",methods=["POST"])
 def signupdata():
-    if "error" in session:
-        session.pop("error")
     username = request.form["username"]
     password = request.form["password"]
     if username == "" or password == "":
-        session["error"]="tunnus tai salasana ei voi olla tyhjä"
-        return redirect("/signup")
+        return render_template("signup.html", error="tunnus tai salasana ei voi olla tyhjä")
 
-    sql = "SELECT tunnus FROM photo_kayttajat WHERE tunnus=:username"
+    sql = "SELECT tunnus FROM photos_kayttajat WHERE tunnus=:username"
     result = db.session.execute(sql, {"username":username})
     user = result.fetchone()
     if user != None:
-        session["error"]="tunnus on jo käytössä"
-        return redirect("/signup")
+        return render_template("signup.html", error="tunnus on jo käytössä")
     
     hash_pw = generate_password_hash(password)
-    sql = "INSERT INTO photo_kayttajat (tunnus,salasana,admin) VALUES (:username,:password,false)"
+    sql = "INSERT INTO photos_kayttajat (tunnus,salasana,admin) VALUES (:username,:password,false)"
     db.session.execute(sql, {"username":username,"password":hash_pw})
     db.session.commit()
 
     session["username"] = username
     return redirect("/")
 
+@app.route("/upload", methods=['GET', 'POST'])
+def upload_photo():
+    if request.method == 'POST':
+        photo = request.files["photo"]
+        if photo.filename == "":
+#            flash('No selected file')
+            return render_template("upload.html", message="Virhe: tiedostoa ei ole valittu")
+        if not photo.filename.endswith(".jpg"):
+            return render_template("upload.html", message="Virhe: vain JPG tiedostoja")
+        img = Image.open(photo)
+        img.thumbnail((800,600))
+        img.save("static/"+app.config["PHOTOFOLDER"]+"2.jpg")
+        return render_template("addinfo.html", filename=url_for("static", filename=app.config["PHOTOFOLDER"] + "2.jpg"), date=img._getexif()[36867])
+    return render_template("upload.html")
+
+@app.route("/addinfo", methods=['GET', 'POST'])
+def addinfo():
+    if request.method == 'POST':
+        pass
+    return render_template("addinfo.html")
+
 @app.route("/logout")
 def logout():
     del session["username"]
     return redirect("/")
+
+def limit_size(img):
+    limit=max(img.shape[0]/600, img.shape[0]/800)
+    if limit>1:
+        return img.resize()
+    return img
--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
-CREATE TABLE photo_kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, admin BOOLEAN);
+CREATE TABLE photos_kayttajat (id SERIAL PRIMARY KEY, tunnus TEXT, salasana TEXT, admin BOOLEAN);
 
